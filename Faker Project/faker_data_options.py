d4a599b3b47a3c084b306985551e7d44f1982bcd
amiran tevzadze <135262055+AmexTevz@users.noreply.github.com>
2023-06-02 18:58:37 -0400

    Update faker_data_options.py

--- Faker Project/faker_data_options.py
+++ Faker Project/faker_data_options.py
@@ -7,16 +7,14 @@ fake = Faker()
 class Options:
     def faker_header(self):
         age = random.randint(18, 66)
-        gender = random.choice(['Male', 'Female'])
 
-        header_dict = {'Fname': fake.first_name(), 'Lname': fake.last_name(), 'Age': age, 'Gender': gender,
+        header_dict = {'Fname': fake.first_name(), 'Lname': fake.last_name(), 'Age': age,
                        'Job': ' '.join(fake.job().split(',')).replace("'s",""),
                        'Company': ' '.join(fake.company().split(',')).replace("'s","")}
         return header_dict
 
     def faker_dict(self):
-        database = {'Fname': 'Fname varchar(500)', 'Lname': 'Lname varchar(500)', 'Age': 'Age int',
-                    'Gender': 'Gender varchar(500)', 'Job': 'Job varchar(500)', 'Company': 'Company varchar (500)'}
+        database = {'Fname': 'Fname varchar(500)', 'Lname': 'Lname varchar(500)', 'Age': 'Age int', 'Job': 'Job varchar(500)', 'Company': 'Company varchar (500)'}
         return database
 
 
